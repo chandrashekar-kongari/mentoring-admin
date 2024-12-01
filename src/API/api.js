98a5a7a546eac422af97493a96699e22124b9a60
kongarichandrashekar8 <kongarichandrashekar8@gmail.com>
2024-12-01 14:05:40 -0500

    draft

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,7 @@
 // const endpoint='https://mentoring-400802.ue.r.appspot.com'
 //above one is working
 // const endpoint='https://dev.techpact.org/flaskapp'
-// const endpoint =process.env.REACT_APP_BACKEND_ENDPOINT;
-const endpoint='http://127.0.0.1:5000'
+const endpoint =process.env.REACT_APP_BACKEND_ENDPOINT;
+// const endpoint='http://127.0.0.1:5000'
 // const endpoint='https://us-central1-notification-services-a7297.cloudfunctions.net/api/'
 export default endpoint
